b3f5ba7666e3373f48bdc866ae09e2843510b7f9
Lee Priest <leeclarkepriest@gmail.com>
2025-11-21 12:34:04 -0500

    Add new methods to IAddin interface

--- IAddin.cs
+++ IAddin.cs
@@ -3,5 +3,11 @@
     public interface IAddin
     {
         void ShowToastNotification(string title, string message, NotificationType notificationType);
+        AddCustomNoteResult AddCustomNote(int sheetIndex, string noteText, int noteIndex = -1, bool subNote = false, string parentId = "", bool suppressTableRefresh = false);
+        DeleteCustomNoteResult DeleteCustomNote(int sheetIndex, string noteId, bool suppressTableRefresh = false);
+        GetSheetNotesResult GetAllSheetNotes(int sheetIndex);
+        void OpenEditor();
+        RefreshNoteTablesResult RefreshNoteTables(int sheetIndex);
+        UpdatedCustomNoteResult UpdateCustomNote(int sheetIndex, string noteId, string newNoteText, bool toggleSubNote = false, bool suppressTableRefresh = false);
     }
 }
