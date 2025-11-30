469b5750fac88ca31903e4d15fa6d09eda8f467e
Lee Priest <leeclarkepriest@gmail.com>
2025-11-30 12:07:46 -0500

    Also update IAddin

--- IAddin.cs
+++ IAddin.cs
@@ -2,7 +2,7 @@
 {
     public interface IAddin
     {
-        void ShowToastNotification(string title, string message, NotificationType notificationType);
+        void ShowToastNotification(string title, string message, NotificationType notificationType, int delaySeconds);
         AddCustomNoteResult AddCustomNote(int sheetIndex, string noteText, int noteIndex = -1, bool subNote = false, string parentId = "", bool suppressTableRefresh = false);
         DeleteCustomNoteResult DeleteCustomNote(int sheetIndex, string noteId, bool suppressTableRefresh = false);
         GetSheetNotesResult GetAllSheetNotes(int sheetIndex);
